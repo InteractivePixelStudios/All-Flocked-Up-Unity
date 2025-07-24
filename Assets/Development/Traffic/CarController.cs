cabb27d30850b4c30d8cd98870f9dfa1229283a4
LordTraxxas <167879743+LordTraxxas@users.noreply.github.com>
2025-07-24 11:58:13 -0400

    more car stuff

--- Assets/Development/Traffic/CarController.cs
+++ Assets/Development/Traffic/CarController.cs
@@ -1,25 +1,61 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Splines;
 
 public class CarController : MonoBehaviour
 {
-    private MeshCollider mC;
-    private NavMeshAgent nMA;
+    [Header("Car Components")]
+    private MeshCollider MC;
+    private SplineAnimate SA;
+
+    [Header("Car Settings")]
+    [SerializeField] public bool isStopped = false;
+    [SerializeField] public float maxSpeed = 10f;
+    [SerializeField] public float acceleration = 2f;
+    [SerializeField] public float currentSpeed = 0f;
+    [SerializeField] public Spline spline;
+
     void Start()
     {
-        if (mC == null)
+        if (MC == null)
         {
-            mC = GetComponent<MeshCollider>();
+            MC = GetComponent<MeshCollider>();
         }
-        if (nMA == null)
+        if (SA == null)
         {
-            nMA = GetComponent<NavMeshAgent>();
+            SA = GetComponent<SplineAnimate>();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SA.MaxSpeed = currentSpeed;
+    }
+
+    public void SetNewSpline(Spline newSpline)
+    {
+        spline = newSpline;
+    }
+
+    public void StartCar()
+    {
+        isStopped = false;
+    }
+
+    public void Accelerate(float deltaTime)
+    {
+        if (isStopped) return;
+        currentSpeed = Mathf.Min(currentSpeed + acceleration * deltaTime, maxSpeed);
+    }
+    public void Decelerate(float deltaTime)
+    {
+        if (isStopped) return;
+        currentSpeed = Mathf.Max(currentSpeed - acceleration * deltaTime, 0f);
+
+        if (currentSpeed == 0f)
+        {
+            isStopped = true;
+        }
     }
 }
