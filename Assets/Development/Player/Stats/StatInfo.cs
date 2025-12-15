3ed72fa46bb85b4b2bc0ba06f541299b5a61d49d
SeabassOlko <63417711+SeabassOlko@users.noreply.github.com>
2025-12-15 12:27:12 -0600

    updates

--- Assets/Development/Player/Stats/StatInfo.cs
+++ Assets/Development/Player/Stats/StatInfo.cs
@@ -1,16 +1,60 @@
-using UnityEngine;
 
-public class StatInfo : MonoBehaviour
+
+using System;
+
+public class StatInfo 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    float health, stamina, stealth, speed, poopAmount, damage;
+
+    public enum stats
     {
-        
+        Health, Stamina, Stealth, Speed, PoopAmount, Damage
     }
 
-    // Update is called once per frame
-    void Update()
+    public float GetStat(stats stat)
     {
-        
+        switch (stat)
+        {
+            case stats.Health:
+                return health;
+            case stats.Stamina:
+                return stamina;
+            case stats.Stealth:
+                return stealth;
+            case stats.Speed:
+                return speed;
+            case stats.PoopAmount:
+                return poopAmount;
+            case stats.Damage:
+                return damage;
+        }
+
+        return 0;
     }
+
+    public void SetStat(stats stat, float value)
+    {
+        switch (stat)
+        {
+            case stats.Health:
+                health = value;
+                break;
+            case stats.Stamina:
+                stamina = value;
+                break;
+            case stats.Stealth:
+                stealth = value;
+                break;
+            case stats.Speed:
+                speed = value;  
+                break;
+            case stats.PoopAmount:
+                poopAmount = value;
+                break;
+            case stats.Damage:
+                damage = value;
+                break;
+        }
+    }
+
 }
--- Assets/Development/Player/Stats/StatManager.cs
+++ Assets/Development/Player/Stats/StatManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class StatManager : Singleton<StatManager>
 {
+    StatInfo _stats;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
