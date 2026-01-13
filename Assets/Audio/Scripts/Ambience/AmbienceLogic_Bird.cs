75e2578fd16db9fc98bf95d3c5ef5f33cbd1ccf4
Isaiah Paterson-McFadden <gunstinger8384@gmail.com>
2026-01-13 14:54:21 -0500

    Push before Merge.

--- Assets/Audio/Scripts/Ambience/AmbienceLogic_Bird.cs
+++ Assets/Audio/Scripts/Ambience/AmbienceLogic_Bird.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class AmbienceLogic_Bird : MonoBehaviour
 {
-    private List<GameObject> treesInArea = new List<GameObject>();
+    [SerializeField] private List<GameObject> treesInArea = new List<GameObject>();
     [SerializeField] EventReference birdChirpEvent; // Assign in Inspector
-    [SerializeField] int numberOfBirds = 11; // This is hard coded, I don't like it but it is needed for now.
-    [SerializeField] GameObject FMODVisualizerObject; // For testing purposes only.
+    [SerializeField] int numberOfBirds = 11; // This is hard coded, I don't like it but it is needed for now, FMOD does not like dynamics.
+    [SerializeField] GameObject FMODVisualizerPrefab; // This is messy for now, just for quick testing.
+    private GameObject FMODVisualizerObject; // For testing purposes only - will be updated with my own custom more advanced visualizer.
     private bool isBirdChirping = false;
 
     public bool freezeScript = false;
 
     void Start()
     {
+        if (treesInArea.Count < 1) { this.enabled = false; return; } // No trees in list, disable script.
 
+        FMODVisualizerObject = Instantiate(FMODVisualizerPrefab);
+        FMODVisualizerObject.name = "FMODVisualizer_BirdAmbience"; // Just a precaution.
+        FMODVisualizerObject.transform.SetParent(this.transform); // Keep hierarchy clean.
+        FMODVisualizerObject.GetComponent<FMODAttenuationGizmo>().eventReference = RuntimeManager.PathToEventReference("event:/Ambience/WorldBirds");
     }
 
     // Update is called once per frame
@@ -25,8 +32,7 @@ public class AmbienceLogic_Bird : MonoBehaviour
         if (freezeScript) // TEMPORARY - For testing purposes.
             return;
 
-        if (treesInArea.Count < 1 || isBirdChirping) // No trees in area or bird is already chirping - either way, we're doing nothing.
-            return;
+        if (isBirdChirping) { return; }  // Already chirping, wait.
 
         int randomNumber = Random.Range(0, treesInArea.Count); // Random index based on number of trees in area.
         GameObject selectedTree = treesInArea[randomNumber]; // Keep track of which tree was selected.
@@ -39,7 +45,7 @@ public class AmbienceLogic_Bird : MonoBehaviour
         StartCoroutine(PlayBirdChirp(selectedTree, randomChirpClip)); // Play the bird chirp at the
     }
 
-    private void OnTriggerEnter(Collider other) // One time trigger.
+    private void OnTriggerEnter(Collider other) // One time trigger. THIS IS NOT USED ANYMORE - SEE NOTES AT BOTTOM
     {
         if (other.CompareTag("Tree"))
         {
@@ -88,3 +94,10 @@ public class AmbienceLogic_Bird : MonoBehaviour
         }
     }
 }
+
+
+// NOTES
+
+// Script Change - Isaiah PM 
+// I have changed this script from being area based trigger to hard coded referecnes to the tress in an area
+// This has been done to reduce the number of triggers and colliders in the scene and to avoid potential issues with multiple triggers overlapping.
