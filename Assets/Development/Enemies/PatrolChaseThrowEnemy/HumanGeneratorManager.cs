6d3fc8ec07d376ac458c7be88387cc48ae9d3e57
SeabassOlko <63417711+SeabassOlko@users.noreply.github.com>
2026-03-25 14:40:27 -0500

    Human Generator
    Summary
    - Human Generator prefab stores all types of bodies and clothing
    - Skin selector changes clothes of the npc from the generator
    - test prefab changed with meshes selected

--- Assets/Development/Enemies/PatrolChaseThrowEnemy/HumanGeneratorManager.cs
+++ Assets/Development/Enemies/PatrolChaseThrowEnemy/HumanGeneratorManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 [System.Serializable]
 public class Clothes
 {
-    public Mesh[] clothes;
+    public SkinnedMeshRenderer[] clothes;
 }
 
 public class HumanGeneratorManager : MonoBehaviour
 {
-    public Mesh[] BodyTypeMeshes;
+    public SkinnedMeshRenderer[] BodyTypeMeshes;
     public Clothes[] skinnyMaleClothes;
     public Clothes[] builtMaleClothes;
     public Clothes[] heavysetMaleClothes;
@@ -19,55 +19,41 @@ public class HumanGeneratorManager : MonoBehaviour
 
     public Mesh[] GenerateHuman()
     {
-        int rand = UnityEngine.Random.Range(0, 6);
+        int rand = UnityEngine.Random.Range(0, 4);
 
         int index = 1;
 
         Mesh[] mesh = new Mesh[6];
 
-        mesh[0] = BodyTypeMeshes[rand];
+        mesh[0] = BodyTypeMeshes[rand].sharedMesh;
 
         switch (rand)
         {
             case 0:
                 foreach (Clothes clothes in skinnyMaleClothes)
                 {
-                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)];
+                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)].sharedMesh;
                     index++;
                 }
                 break;
             case 1:
-                foreach (Clothes clothes in builtMaleClothes)
+                foreach (Clothes clothes in heavysetMaleClothes)
                 {
-                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)];
+                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)].sharedMesh;
                     index++;
                 }
                 break;
             case 2:
-                foreach (Clothes clothes in heavysetMaleClothes)
+                foreach (Clothes clothes in skinnyFemaleClothes)
                 {
-                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)];
+                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)].sharedMesh;
                     index++;
                 }
                 break;
             case 3:
-                foreach (Clothes clothes in skinnyFemaleClothes)
-                {
-                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)];
-                    index++;
-                }
-                break;
-            case 4:
-                foreach (Clothes clothes in builtFemaleClothes)
-                {
-                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)];
-                    index++;
-                }
-                break;
-            case 5:
                 foreach (Clothes clothes in heavysetFemaleClothes)
                 {
-                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)];
+                    mesh[index] = clothes.clothes[UnityEngine.Random.Range(0, clothes.clothes.Length)].sharedMesh;
                     index++;
                 }
                 break;
