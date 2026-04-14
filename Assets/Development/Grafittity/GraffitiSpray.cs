f65cce16692072ad94064b7113cf89d0cda76e02
SJP <jsands180@gmail.com>
2026-04-13 21:52:52 -0400

    changed new texture conditions
    Basically takes the decal's texture on the projector (if possible) and uses that as a canvas. otherwise just makes an empty texture 2d. Also gives the paint texture a name, painter.

--- Assets/Development/Grafittity/GraffitiSpray.cs
+++ Assets/Development/Grafittity/GraffitiSpray.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class GraffitiSpray : MonoBehaviour
@@ -12,20 +13,34 @@ public class GraffitiSpray : MonoBehaviour
     void Start()
     {
         projector = GetComponent<DecalProjector>();
-
-        paintTexture = new Texture2D(textureSize, textureSize, TextureFormat.RGBA32, false);
-
-        // Start transparent
-        Color[] pixels = new Color[textureSize * textureSize];
-        for (int i = 0; i < pixels.Length; i++)
-            pixels[i] = Color.clear;
-
-        paintTexture.SetPixels(pixels);
-        paintTexture.Apply();
+        // Grab the original texture from the material
+        Texture2D original = projector.material.GetTexture("Base_Map") as Texture2D;
+        
+        Debug.Log($"Original texture: {original}, isReadable: {original?.isReadable}");
+        //the texture2D used by the projector material needs to be read/write enabled in advanced settings, or else it won't work!!!
 
         // Duplicate material so it's unique
         projector.material = new Material(projector.material);
-        projector.material.SetTexture("_BaseMap", paintTexture);
+        if (original != null && original.isReadable)
+        {
+            // Copy the existing texture so we paint on top of it
+            paintTexture = new Texture2D(original.width, original.height, TextureFormat.RGBA32, false);
+            paintTexture.SetPixels(original.GetPixels());
+            paintTexture.Apply();
+        }
+        else
+        {
+            // Fallback, start with a transparent canvas
+            paintTexture = new Texture2D(textureSize, textureSize, TextureFormat.RGBA32, false);
+            Color[] pixels = new Color[textureSize * textureSize];
+            for (int i = 0; i < pixels.Length; i++)
+                pixels[i] = Color.clear;
+            paintTexture.SetPixels(pixels);
+            paintTexture.Apply();
+        }
+
+        paintTexture.name = "painter";
+        projector.material.SetTexture("Base_Map", paintTexture);
     }
 
     public void Paint(Vector2 uv, Color color, int brushSize)
@@ -37,7 +52,7 @@ public class GraffitiSpray : MonoBehaviour
         {
             for (int j = -brushSize; j <= brushSize; j++)
             {
-                Debug.Log($"Checking pixel offset ({i}, {j})");
+                //Debug.Log($"Checking pixel offset ({i}, {j})");
                 float dist = Mathf.Sqrt(i * i + j * j);
                 if (dist > brushSize) continue;
 
