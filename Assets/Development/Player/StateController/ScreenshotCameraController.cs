0b1aa6eaf47289f997f513e82697588e1c070e98
SJP <jsands180@gmail.com>
2026-04-27 18:20:31 -0400

    screenshotcontroller now gets main cam on start

--- Assets/Development/Player/StateController/ScreenshotCameraController.cs
+++ Assets/Development/Player/StateController/ScreenshotCameraController.cs
@@ -14,6 +14,7 @@ public class ScreenshotCameraController : MonoBehaviour
     private CinemachineBrain cinemachineBrain;
     void Start()
     {
+        cam =  Camera.main;
         cameraCollider = cam.GetComponent<Collider>();
         cinemachineBrain = cam.GetComponent<CinemachineBrain>();
 
