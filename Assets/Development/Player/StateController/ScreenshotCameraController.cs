172d5339c5f81feda60b3d27dbba4524a0aeeefd
SJP <jsands180@gmail.com>
2026-05-03 21:55:10 -0400

    added selfie cam mode + ui toggle
    added secondary selfie cam mode. Also added reference to UI hud controller, and function calls to enable/disable the hud + camera ui while entering and exiting camera mode. Also added empty methods for camera functions (WIP)

--- Assets/Development/Player/StateController/ScreenshotCameraController.cs
+++ Assets/Development/Player/StateController/ScreenshotCameraController.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 using UnityEngine.InputSystem; // only needed temporarily
 using Unity.Cinemachine;
+using UnityEngine.Serialization;
 
 
 public class ScreenshotCameraController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private PlayerStateController psc;
-    [SerializeField] private Transform camAnchor;
+    [SerializeField] private Transform camAnchorFront;
+    [SerializeField] private Transform camAnchorBack;
+    [FormerlySerializedAs("UI_Hud")] [SerializeField] private UI_HudController uiHud;
+    
+    
+    private bool isSelfie = false;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Collider cameraCollider;
     private CinemachineBrain cinemachineBrain;
     void Start()
     {
+        
         cam =  Camera.main;
         cameraCollider = cam.GetComponent<Collider>();
         cinemachineBrain = cam.GetComponent<CinemachineBrain>();
+        camAnchorFront = transform.Find("FrontCamAnchor");
+        camAnchorBack = transform.Find(("SelfieCamAnchor"));
+        uiHud = FindAnyObjectByType<UI_HudController>();
+
+        if (!camAnchorFront)
+            Debug.Log("camAnchorFront is null");
+        if (!camAnchorBack)
+            Debug.Log("camAnchorBack is null");
+        
 
     }
 
@@ -36,6 +53,19 @@ public class ScreenshotCameraController : MonoBehaviour
             else
                 StartPhotoMode();
         }
+        if (psc.CurrentState == PlayerState.PhotoMode)
+        {
+            Debug.Log("in photo mode, waiting for space");
+            //change this later as well for inputAction system
+            
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                Debug.Log("space pressed, flipping");
+                FlipCamera();
+            }
+        }
+
+        
        
     }
 
@@ -45,14 +75,17 @@ public class ScreenshotCameraController : MonoBehaviour
        Debug.Log("Photo Mode Entered");
 
        if (cameraCollider)
-       { Debug.Log("Main Camera Collider Component disabled by StartPhotoMode"); cameraCollider.enabled = false;}
+       { Debug.Log("Main Camera Collider Component disabled by Photo Mode"); cameraCollider.enabled = false;}
        if (cinemachineBrain)
-       { Debug.Log("Main Camera CinemachineBrain Component disabled by StartPhotoMode"); cinemachineBrain.enabled = false;}
+       { Debug.Log("Main Camera CinemachineBrain Component disabled by Photo Mode"); cinemachineBrain.enabled = false;}
 
        //move cam to anchor
-       cam.transform.parent = camAnchor;
+       cam.transform.parent = camAnchorFront;
        cam.transform.localPosition = Vector3.zero;
        cam.transform.localRotation = Quaternion.identity;
+       
+       uiHud.ToggleMainHUD(false);
+       uiHud.ToggleCameraOverlay(true);
     }
 
     private void EndPhotoMode()
@@ -60,12 +93,38 @@ public class ScreenshotCameraController : MonoBehaviour
         psc.ExitPhotoMode();
         Debug.Log("Photo Mode Exited");
         
-        { Debug.Log("Main Camera Collider Component enabled by StartPhotoMode"); cameraCollider.enabled = true;}
-        { Debug.Log("Main Camera CinemachineBrain Component enabled by StartPhotoMode"); cinemachineBrain.enabled = true;}
+        { Debug.Log("Main Camera Collider Component enabled by Photo Mode"); cameraCollider.enabled = true;}
+        { Debug.Log("Main Camera CinemachineBrain Component enabled by Photo Mode"); cinemachineBrain.enabled = true;}
 
         //reset to normal camera control
         cam.transform.parent = null;
+
+        //reset to default camera orientation for future camera angles
+        isSelfie = false;
+        
+        uiHud.ToggleMainHUD(true);
+        uiHud.ToggleCameraOverlay(false);
     }
     
+    private void FlipCamera()
+    {
+        isSelfie = !isSelfie;
+        Transform target = isSelfie ? camAnchorBack : camAnchorFront;
+        cam.transform.parent = target;
+        cam.transform.localPosition = Vector3.zero;
+        cam.transform.localRotation = Quaternion.identity;
+    }
+    
+    
+    
+    
+    private void TakePhoto()
+    {}
+    
+    private void OrientCamera()
+    {}
+    
+    private void ZoomCamera()
+    {}
     
 }
